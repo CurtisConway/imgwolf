c660b8e29d3f09a0d9c4bf1629482015819f5f4a
Curtis <curtis@drumeo.com>
2019-04-08 22:09:12 -0700

    Expose json api

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-require('dotenv').config();
 const express = require('express');
-require('express-async-errors');
+const AWS = require('aws-sdk');
+const helmet = require('helmet');
+const auth = require('./src/routes/auth');
+
+require('dotenv').config();
+// require('express-async-errors');
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 const app = express();
 const port  = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/api/auth', auth);
 
 const server = app.listen(port, () => console.log(`Listening on port ${ port }`));
 
