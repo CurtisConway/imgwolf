c7883c3a82175f6cd39f53271a528ce413d38659
Curtis <curtis@drumeo.com>
2019-04-09 21:26:58 -0700

    Incorrect validation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,7 +22,7 @@ router.post('/', async (req, res) => {
 function validate(req){
     const schema = {
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(6).max(1024).required(),
+        password: Joi.string().min(8).max(1024).required(),
     };
 
     return Joi.validate(req, schema);
