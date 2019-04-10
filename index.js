818430a4877bedbe838adae6b001de3cf4ad068f
Curtis <curtis@drumeo.com>
2019-04-09 21:22:51 -0700

    Add startup scripts

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express');
-const AWS = require('aws-sdk');
-const helmet = require('helmet');
-const auth = require('./src/routes/auth');
+// const AWS = require('aws-sdk');
 
 require('dotenv').config();
-// require('express-async-errors');
+require('express-async-errors');
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+// AWS.config.update({
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
 
 const app = express();
 const port  = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(helmet());
-app.use('/api/auth', auth);
+require('./startup/db')();
+require('./startup/logger')();
+require('./startup/routes')(app);
+require('./startup/validation')();
 
-const server = app.listen(port, () => console.log(`Listening on port ${ port }`));
+const server = app.listen(port, () => console.log(`Listening on port ${ port }...`));
 
 module.exports = server;
