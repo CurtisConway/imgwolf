205ca458a056c6bc8832855b5ac30bf946ede4bb
Curtis <curtis@drumeo.com>
2019-04-09 21:21:56 -0700

    Add routes

---
+++ startup/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const helmet = require('helmet');
+const auth = require('../src/routes/auth');
+
+module.exports = function(app){
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static('public'));
+    app.use(helmet());
+    
+    app.use('/api/auth', auth);
+
+    app.use(error);
+};
