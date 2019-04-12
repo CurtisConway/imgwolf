2f085f4d2c89abba56a9747e3691d42fd0cf1b29
Curtis <curtis@drumeo.com>
2019-04-11 22:35:08 -0700

    Removed min validation from password for client sign in

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -23,7 +23,7 @@ router.post('/', async (req, res) => {
 function validate(req) {
     const schema = {
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(8).max(1024).required(),
+        password: Joi.string().max(1024).required(),
     };
 
     return Joi.validate(req, schema);
