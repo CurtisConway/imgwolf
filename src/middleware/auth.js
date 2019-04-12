389494fe8c1bbffcc0f9fef656d65bfd6a2c4545
Curtis <curtis@drumeo.com>
2019-04-11 22:59:16 -0700

    Attach the token onto the request

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = async function (req, res, next) {
     const token = req.header('x-auth-token');
     if(!token) return res.status(401).send('Access denied. No token provided.');
 
-    await refreshUserToken(token)
+    const refresh = await refreshUserToken(token)
         .catch(err => {
             if(err.code === 'TOKEN_EXPIRED'){
                 return res.status(403).send('Session Expired. Please Log in Again');
@@ -13,5 +13,6 @@ module.exports = async function (req, res, next) {
             return res.status(401).send('Access denied. User not authorized.');
         });
 
+    req.newRefreshToken = refresh.data.refresh_token;
     next();
 };
