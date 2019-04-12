aabc7c0e2cc205b39025084d131a28879c879ac3
Curtis <curtis@drumeo.com>
2019-04-11 22:26:45 -0700

    Reformat

--- startup/routes.js
+++ startup/routes.js
@@ -3,9 +3,9 @@ const helmet = require('helmet');
 const auth = require('../src/routes/auth');
 const error = require('../src/middleware/error');
 
-module.exports = function(app){
+module.exports = function (app) {
     app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+    app.use(express.urlencoded({extended: true}));
     app.use(express.static('public'));
     app.use(helmet());
 
