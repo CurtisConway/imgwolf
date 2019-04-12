cbf9f70b4d7ac60c8875b2aea6daf55ffcd0dc2d
Curtis <curtis@drumeo.com>
2019-04-11 22:31:25 -0700

    Add aws startup

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require('express');
-// const AWS = require('aws-sdk');
 
 require('dotenv').config();
 require('express-async-errors');
 
-// AWS.config.update({
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-// });
-
 const app = express();
-const port  = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 require('./startup/db')();
 require('./startup/logger')();
 require('./startup/routes')(app);
+require('./startup/aws')();
 require('./startup/validation')();
 
-const server = app.listen(port, () => console.log(`Listening on port ${ port }...`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
