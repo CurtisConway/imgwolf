558b4a763e645e9363e7d02deade2e348a05c35e
Curtis <curtis@drumeo.com>
2019-04-11 22:47:58 -0700

    Add auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,17 @@
+const { refreshUserToken } = require('../services/firebase');
+
+module.exports = async function (req, res, next) {
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).send('Access denied. No token provided.');
+
+    await refreshUserToken(token)
+        .catch(err => {
+            if(err.code === 'TOKEN_EXPIRED'){
+                return res.status(403).send('Session Expired. Please Log in Again');
+            }
+
+            return res.status(401).send('Access denied. User not authorized.');
+        });
+
+    next();
+};
