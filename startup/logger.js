81c3fe310185144fba9b9fe10ba7ae47229e8ca6
Curtis <curtis@drumeo.com>
2019-04-12 21:45:33 -0700

    Logger not working

--- startup/logger.js
+++ startup/logger.js
@@ -3,25 +3,27 @@ require('winston-mongodb');
 
 module.exports = function () {
     winston.createLogger({
+        level: 'error',
         format: winston.format.combine(
             winston.format.prettyPrint(),
             winston.format.timestamp()
         ),
         transports: [
+            new winston.transports.File({
+                filename: 'error.log',
+                level: 'error',
+                handleExceptions: true,
+            }),
             new winston.transports.Console({
                 handleExceptions: true,
             }),
-            new winston.transports.File({
-                filename: 'error.log',
-                handleExceptions: true,
-            }),
-            new winston.transports.MongoDB({
-                db: process.env.DB_HOST,
-                handleExceptions: true,
-                options: {
-                    useNewUrlParser: true
-                }
-            })
+            // new winston.transports.MongoDB({
+            //     db: process.env.DB_HOST,
+            //     handleExceptions: true,
+            //     options: {
+            //         useNewUrlParser: true
+            //     }
+            // })
         ],
     });
 
