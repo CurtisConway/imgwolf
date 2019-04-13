e2d40a5ea7e8784128b03accd8a5fd35befa10f6
Curtis <curtis@drumeo.com>
2019-04-12 21:47:40 -0700

    Add firebase startup

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-
-require('dotenv').config();
 require('express-async-errors');
+require('dotenv').config();
 
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-require('./startup/db')();
+// require('./startup/db')();
 require('./startup/logger')();
+require('./startup/firebase')();
 require('./startup/routes')(app);
 require('./startup/aws')();
 require('./startup/validation')();
