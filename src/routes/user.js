0662744c4742480f065d43b402f6724d9d88d39a
Curtis <curtis@drumeo.com>
2019-04-13 13:07:23 -0700

    Add update user route and test

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { getUserByUid } = require('../services/firebase');
+const Joi = require('joi');
+const { getUserByUid, updateUserByUid } = require('../services/firebase');
 
 router.get('/', auth, async (req, res) => {
     let user;
-
     try {
         user = await getUserByUid(req.currentUserId);
     } catch(err){
@@ -22,6 +22,39 @@ router.get('/', auth, async (req, res) => {
         }
     });
 });
+
+router.post('/update', auth, async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    let user;
+    try {
+        user = await updateUserByUid(req.currentUserId, req.body);
+    } catch(err){
+        return res.status(404).send('User Not Found');
+    }
+
+    return res.status(200).send({
+        user: {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName || '',
+            photoURL: user.photoURL || '',
+            verified: user.emailVerified,
+        }
+    });
+});
+
+function validate(req){
+    const schema = {
+        displayName: Joi.string().min(3).max(50),
+        photoURL: Joi.string().max(1024).uri(),
+    };
+
+    return Joi.validate(req, schema);
+}
 
 module.exports = router;
 
--- tests/integration/user.test.js
+++ tests/integration/user.test.js
@@ -5,20 +5,12 @@ describe('/api/user', () => {
     let server;
     let sessionCookie;
 
-    const exec = async () => {
-        return await request(server)
-            .get('/api/user')
-            .set('Cookie', `session=${sessionCookie}`)
-            .set('Accept', 'application/json')
-            .send();
-    };
-
     beforeEach(async () => {
         server = require('../../index');
 
         const user = await signInWithEmailAndPassword({
-            email: 'test@email.com',
-            password: 'password'
+            email: process.env.TEST_EMAIL,
+            password: process.env.TEST_PASSWORD
         });
 
         const idToken = await user.user.getIdToken();
@@ -30,14 +22,15 @@ describe('/api/user', () => {
     });
 
     describe('GET /', () => {
-        it('returns a 401 response with no session cookie', async () => {
-            sessionCookie = '';
-            const response = await exec();
+        const exec = async () => {
+            return await request(server)
+                .get('/api/user')
+                .set('Cookie', `session=${sessionCookie}`)
+                .set('Accept', 'application/json')
+                .send();
+        };
 
-            expect(response.status).toBe(401);
-        });
-
-        it('returns a 403 response an expired session cookie', async () => {
+        it('returns a 403 response with an expired session cookie', async () => {
             sessionCookie = 'eyJhbGciOiJSUzI1NiIsImtpZCI6InNrSUJOZyJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS9pbWd3b2xmIiwiYXVkIjoiaW1nd29sZiIsImF1dGhfdGltZSI6MTU1NTEyNzkzOCwidXNlcl9pZCI6ImVUVlRvbTNmUFJVeXRiYWxHNXF6RzREUnlScDEiLCJzdWIiOiJlVFZUb20zZlBSVXl0YmFsRzVxekc0RFJ5UnAxIiwiaWF0IjoxNTU1MTI3OTM5LCJleHAiOjE1NTUxMjgyOTksImVtYWlsIjoidGVzdEBlbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEBlbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Zor0cWwKa4k1fnsYlZSheGxdpC8njd1wqx5bdM4dvFK1D0BRAhYllwPQ3gB1Zqr4RDnzYK9_ths_WWqGgdwoi1YlK2CuMcR3TIonAK5XSchn65RGzlWoylqeM7RFfWN0gMJyRw4ylO69Sy3y5XO5m8DD_BSII_KvYXpUVm2R4JrUv049vjduBcWgRZUy2gHG4_TYKpCUUPa8Coiqx3iJ1pE7jGJ5TPdRqdInnjHy7SGK0YONM_sHDRekw5hm6EululD9hgJZIyiNxRtde8udFL_zCFWenmyxGuSNc1NgQVN8ushoZ1-fxWuPdIhlLWuDe9_SifajModHQ1AAaqksCA';
 
             const response = await exec();
@@ -45,14 +38,60 @@ describe('/api/user', () => {
             expect(response.status).toBe(403);
         });
 
-        it('returns a 401 response an invalid session cookie', async () => {
-            sessionCookie = 'abc';
+        it('returns a 200 response with user details', async () => {
             const response = await exec();
 
-            expect(response.status).toBe(401);
+            expect(response.status).toBe(200);
+            expect(response.body.user).toHaveProperty('uid');
+            expect(response.body.user).toHaveProperty('email');
+            expect(response.body.user).toHaveProperty('displayName');
+            expect(response.body.user).toHaveProperty('photoURL');
+            expect(response.body.user).toHaveProperty('verified');
+        });
+    });
+
+    describe('POST /update', () => {
+        let displayName;
+        let photoURL;
+
+        const exec = async () => {
+            return await request(server)
+                .post('/api/user/update')
+                .set('Cookie', `session=${sessionCookie}`)
+                .set('Accept', 'application/json')
+                .send({displayName, photoURL});
+        };
+
+        it('returns a 403 response with an expired session cookie', async () => {
+            sessionCookie = 'eyJhbGciOiJSUzI1NiIsImtpZCI6InNrSUJOZyJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS9pbWd3b2xmIiwiYXVkIjoiaW1nd29sZiIsImF1dGhfdGltZSI6MTU1NTEyNzkzOCwidXNlcl9pZCI6ImVUVlRvbTNmUFJVeXRiYWxHNXF6RzREUnlScDEiLCJzdWIiOiJlVFZUb20zZlBSVXl0YmFsRzVxekc0RFJ5UnAxIiwiaWF0IjoxNTU1MTI3OTM5LCJleHAiOjE1NTUxMjgyOTksImVtYWlsIjoidGVzdEBlbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEBlbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.Zor0cWwKa4k1fnsYlZSheGxdpC8njd1wqx5bdM4dvFK1D0BRAhYllwPQ3gB1Zqr4RDnzYK9_ths_WWqGgdwoi1YlK2CuMcR3TIonAK5XSchn65RGzlWoylqeM7RFfWN0gMJyRw4ylO69Sy3y5XO5m8DD_BSII_KvYXpUVm2R4JrUv049vjduBcWgRZUy2gHG4_TYKpCUUPa8Coiqx3iJ1pE7jGJ5TPdRqdInnjHy7SGK0YONM_sHDRekw5hm6EululD9hgJZIyiNxRtde8udFL_zCFWenmyxGuSNc1NgQVN8ushoZ1-fxWuPdIhlLWuDe9_SifajModHQ1AAaqksCA';
+
+            const response = await exec();
+
+            expect(response.status).toBe(403);
         });
 
-        it('returns a 200 response with a valid idToken and user details', async () => {
+        it('returns a 400 response if display name does not pass validation', async () => {
+            displayName = 'a';
+            photoURL = 'https://placehold.it/250x250';
+
+            const response = await exec();
+
+            expect(response.status).toBe(400);
+        });
+
+        it('returns a 400 response if photo url does not pass validation', async () => {
+            displayName = 'Test User';
+            photoURL = 'a';
+
+            const response = await exec();
+
+            expect(response.status).toBe(400);
+        });
+
+        it('returns a 200 response with user details', async () => {
+            displayName = 'Test User';
+            photoURL = 'https://placehold.it/250x250';
+
             const response = await exec();
 
             expect(response.status).toBe(200);
