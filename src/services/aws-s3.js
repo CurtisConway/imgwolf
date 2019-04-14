347b702ce8caddcd49c4239ea4e664bcd2fc930d
Curtis <curtis@drumeo.com>
2019-04-14 00:30:19 -0700

    Add AWS s3 service

---
+++ src/services/aws-s3.js
@@ -0,0 +1,85 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: {
+        Bucket: 'imgwolf'
+    }
+});
+
+/**
+ * Upload an image to S3
+ *
+ * @param data {Object}
+ * @returns {Promise}
+ */
+async function uploadImage({title, file, collection}){
+    const uploadedFile = await readFile(file);
+    const imageExtension = file.mimetype.split('/')[1];
+
+    fs.unlink(file.path, (err) => { if(err) new Error('Failed to delete temporary file')});
+
+    return await s3Upload({
+        key: `imgwolf/${collection}/${title}.${imageExtension}`,
+        body: uploadedFile
+    });
+}
+
+/**
+ * Delete an image on s3
+ *
+ * @param path {String}
+ * @returns {Promise}
+ */
+async function deleteImage(path){
+    return new Promise((resolve, reject) => {
+        s3.deleteObject({Key: path}, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+/**
+ * Promise wrapper around the fs.readFile method
+ *
+ * @param file {Object}
+ * @returns {Promise}
+ */
+function readFile(file){
+    return new Promise((resolve, reject) => {
+        fs.readFile(file.path, (err, data) => {
+            if(err){
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+/**
+ * Promise wrapper around the s3 upload method
+ *
+ * @param key {String}
+ * @param body {File}
+ * @returns {Promise}
+ */
+function s3Upload({key, body}){
+    return new Promise((resolve, reject) => {
+        s3.upload({Key: key, Body: body, ACL: 'public-read'}, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+module.exports.uploadImage = uploadImage;
+module.exports.deleteImage = deleteImage;
--- startup/aws.js
+++ startup/aws.js
@@ -3,6 +3,8 @@ const AWS = require('aws-sdk');
 module.exports = function(){
     AWS.config.update({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: 'us-east-1',
+        apiVersion: '2014-10-01',
     });
 };
